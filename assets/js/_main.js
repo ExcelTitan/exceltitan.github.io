e8f41de2c988cba71fb24517a88a3a38e3178d22
ExcelTitan <32754674+ExcelTitan@users.noreply.github.com>
2020-03-19 19:46:00 +1100

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -18,6 +18,27 @@ $(document).ready(function() {
   // FitVids init
   $("#main").fitVids();
 
+  // select contents
+  function selectElementContents(el) {
+        var body = document.body, range, sel;
+        if (document.createRange && window.getSelection) {
+            range = document.createRange();
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            try {
+                range.selectNodeContents(el);
+                sel.addRange(range);
+            } catch (e) {
+                range.selectNode(el);
+                sel.addRange(range);
+            }
+        } else if (body.createTextRange) {
+            range = body.createTextRange();
+            range.moveToElementText(el);
+            range.select();
+        }
+    };
+  
   // Sticky sidebar
   var stickySideBar = function() {
     var show =
